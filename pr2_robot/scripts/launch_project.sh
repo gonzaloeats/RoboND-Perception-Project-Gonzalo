f972ced58db76559fb6a4f8ff9426ce6da633812
gonzaloeats <gonzaloescobar2000@gmail.com>
2018-03-04 14:03:03 -0500

    updated to work with new catkin_make

--- pr2_robot/scripts/launch_project.sh
+++ pr2_robot/scripts/launch_project.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 # This script safely launches ros nodes with buffer time to allow param server population
+# this will launch pick and place project but you will have to change testscene
+# manually in the code.
 x-terminal-emulator -e roslaunch pr2_robot pick_place_project.launch & sleep 10 &&
 x-terminal-emulator -e rosrun pr2_robot project.py
